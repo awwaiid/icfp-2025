db0b639a0af216218f30b2b777ba69e808886a7c
Brock Wilcox <awwaiid@thelackthereof.org>
2025-09-05 09:32:19 -0400

    Register

---
+++ misc/register.sh
@@ -0,0 +1,7 @@
+curl -v -X POST -H 'Content-Type: application/json' -d '{
+  "name": "ootbo",
+  "pl": "python, etc",
+  "email": "awwaiid@thelackthereof.org"
+}' https://31pwr5t6ij.execute-api.eu-west-2.amazonaws.com/register
+
+# {"id":"awwaiid@thelackthereof.org zFPVkfKRKAgDUdmVr2Oi1A"}
